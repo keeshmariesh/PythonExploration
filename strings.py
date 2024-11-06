460e7f14913a24d26eeddaacee615c5c02e67e31
keeshmariesh <kumarpatel25@gmail.com>
2024-11-05 22:52:25 -0500

    strings complete

--- strings.py
+++ strings.py
@@ -3,3 +3,9 @@ print('Python is fun')
 print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world") #string concatenation
+greeting = "Hello"
+name = input("Please enter your name ") #input function will write argument as text and then take string input
+print(greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
