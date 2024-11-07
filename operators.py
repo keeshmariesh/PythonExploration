db2393d4f1e0a4e6c73970bbfc79108fe80237e0
keeshmariesh <kumarpatel25@gmail.com>
2024-11-06 20:32:32 -0500

    operators

--- operators.py
+++ operators.py
@@ -10,5 +10,13 @@ print(a % b)    # 0 modulo: the remainder after integer division
 
 print()
 
-for i in range(1, a // b):
-    print(i)
+print(a+b/3-4*12)
+print(a+(b / 3) - (4*12))
+print((((a+b)/3)-4)*12)
+print((((a+b)/3)-4)*12)
+
+c = a + b
+d = c / 3
+e = d - 4
+
+print(e * 12)
