2ef1c93bf455a8a361dba838c5eff20f540d7ddf
keeshmariesh <kumarpatel25@gmail.com>
2024-11-08 12:12:29 -0500

    sequence_operators and sliceback commit

---
+++ sequence_operators.py
@@ -0,0 +1,22 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+# Two ways to concatenate
+print(string1 + string2 + string3 +string4 + string5)
+
+print("he's " "probably " "pining " "for the " "fjords")
+
+print("Hello " * 5)
+
+print("Hello " * (5+4))
+print("Hello " * 5 + "4")
+print("Hello " * 5 + str(4))
+
+today = "friday"
+print("day" in today)   # True
+print("fri" in today)   # True
+print("thur" in today)  # False
+print("parrot" in "fjord")  # False
--- sliceback.py
+++ sliceback.py
@@ -11,8 +11,7 @@ print(backwards3)
 print(letters[16:13:-1])
 print(letters[4::-1])
 print(letters[:-9:-1])
+
 print(letters[-4:])
 print(letters[-1:])
 print(letters[:1])
-
-
