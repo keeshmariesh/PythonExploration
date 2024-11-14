d34b39e262ad1983e5e5e237c1e3523817f508ec
keeshmariesh <kumarpatel25@gmail.com>
2024-11-14 00:07:41 -0500

    Iterating Backwards Algos. Timing Demo

---
+++ LearningPython5_Lists&Tuples/gobackwards.py
@@ -0,0 +1,20 @@
+data = [104, 101, 4, 105, 308, 103, 5,
+        107, 100, 306, 106, 102, 108]
+min_valid = 100
+max_valid = 200
+
+# One way to remove backwards in a sequence
+# for index in range(len(data) - 1, - 1, - 1):
+#     if data[index] < min_valid or data[index] > max_valid:
+#         print(index, data)
+#         del data[index]
+
+# Another way to iterate backwards and remove elements from a sequence
+# Using enumerate is more efficient than using those index calls above
+top_index = len(data) - 1
+for index, value in enumerate(reversed(data)):
+    if value < min_valid or value > max_valid:
+        print(top_index - index, value)
+        del data[top_index - index]
+
+print(data)
